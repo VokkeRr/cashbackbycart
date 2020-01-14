7c0eba6752475856080630bc496f38b04d582104
Vokker <student@localhosts>
2020-01-14 14:18:53 +0300

    fixes #3

--- src/js/app.js
+++ src/js/app.js
@@ -6,26 +6,10 @@ const ans = 0;
 const ans1 = 0;
 const ans2 = 0;
 const ans3 = 0;
-const REGULAR_CASHBACK_PERCENT = 1;
-const INCREASED_CASHBACK_PERCENT = 5;
-const SPECIAL_CASHBACK_PERCENT = 30;
-
-
-if (categ === 'regular') {
-    ans = sum * REGULAR_CASHBACK_PERCENT;
-}
-if (categ === 'increased') {
-    ans = sum * INCREASED_CASHBACK_PERCENT;
-}
-if (categ === 'special') {
-    ans = sum * SPECIAL_CASHBACK_PERCENT;
-}
+const REGULAR_CASHBACK_PERCENT = 0.01;
+const INCREASED_CASHBACK_PERCENT = 0.05;
+const SPECIAL_CASHBACK_PERCENT = 0.3;
 const CASHBACK_LIMIT = 3000;
-
-if (ans > CASHBACK_LIMIT) {
-    ans = CASHBACK_LIMIT;
-}
-console.log(ans);
 
 const purchases = [
     {
@@ -44,38 +28,19 @@ const purchases = [
 
 let cashback = 0;
 
-if (obj.obj1.categ === 'regular') {
-    ans1 = sum * REGULAR_CASHBACK_PERCENT;
+for (const purchase of purchases) {
+    if (purchase.category == REGULAR_CATEGORY) {
+        cashback = (purchase.amount * REGULAR_CASHBACK_PERCENT);
+    } else if (purchase.category == INCREASED_CATEGORY) {
+        cashback = (purchase.amount * INCREASED_CASHBACK_PERCENT);
+    } else if (purchase.category == SPECIAL_CATEGORY) {
+        cashback = (purchase.amount * SPECIAL_CASHBACK_PERCENT);
+    }
 }
-if (obj.obj1.categ === 'increased') {
-    ans1 = sum * INCREASED_CASHBACK_PERCENT;
+if (cashback > CASHBACK_LIMIT){
+    cashback = CASHBACK_LIMIT;
 }
-if (obj.obj1.categ === 'special') {
-    ans1 = sum * SPECIAL_CASHBACK_PERCENT;
-}
-
-//  if obj 2
-if (obj.obj2.categ === 'regular') {
-    ans2 = sum * REGULAR_CASHBACK_PERCENT;
-}
-if (obj.obj2.categ === 'increased') {
-    ans2 = sum * INCREASED_CASHBACK_PERCENT;
-}
-if (obj.obj2.categ === 'special') {
-    ans2 = sum * SPECIAL_CASHBACK_PERCENT;
-}
-// if obj 3
-if (obj.obj3.categ === 'regular') {
-    ans3 = sum * REGULAR_CASHBACK_PERCENT;
-}
-if (obj.obj3.categ === 'increased') {
-    ans3 = sum * INCREASED_CASHBACK_PERCENT;
-}
-if (obj.obj3.categ === 'special') {
-    ans3 = sum * SPECIAL_CASHBACK_PERCENT;
-}
-
-console.log(ans);
+    console.log(ans);
 console.log(ans1);
 console.log(ans2);
 console.log(ans3);
